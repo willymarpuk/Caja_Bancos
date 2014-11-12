6bd4b525bab96173d2c0be99fdbf88140587ced1
willymarpuk <willymarpuk@gmail.com>
2014-11-12 09:07:03 -0300

    pull request de ahalaburda/caja_bancos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,16 +18,49 @@
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require moment
+//= require moment/es
 //= require bootstrap-datetimepicker
+//= require autoNumeric
 //= require_tree .
+
 
 $(document).ready(function() {
 
 	$('.datepicker').datetimepicker({
-	    language: 'ru'
+		autoclose:true,
+		language:'es',
+		weekStart: '0'
 	});
-	$('#table').dataTable()
-	sPaginationType: "full_numbers"
+	$('#table').dataTable({
+		sPaginationType: "full_numbers",
+		bJQueryUI: true,
+		"language": {
+		    "sProcessing":     "Procesando...",
+		    "sLengthMenu":     "Mostrar _MENU_ registros",
+		    "sZeroRecords":    "No se encontraron resultados",
+		    "sEmptyTable":     "Ningún dato disponible en esta tabla",
+		    "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+		    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+		    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+		    "sInfoPostFix":    "",
+		    "sSearch":         "Buscar:",
+		    "sUrl":            "",
+		    "sInfoThousands":  ",",
+		    "sLoadingRecords": "Cargando...",
+
+		    "oPaginate": {
+		        "sFirst":    "Primero",
+		        "sLast":     "Último",
+		        "sNext":     "Siguiente",
+		        "sPrevious": "Anterior"
+		    },
+	    "oAria": {
+	        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+	        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+	    }}
+	});
+
+	$('input[data-role=money]').autoNumeric('init');
 });
 
 $(document).ready(function() {
